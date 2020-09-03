3f7cbc1d117bfc683ab18a070a43953f67aaefed
Philipp Piwo <github@piwo.space>
2020-09-03 17:45:43 +0200

    add sample token sale

--- .scripts/postinstall.js
+++ .scripts/postinstall.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
 
-const exampleContract = fs.readFileSync(__dirname + '/../contracts/ExampleContract.aes', 'utf-8');
-
-fs.writeFileSync(__dirname + '/../ExampleContract.aes.js', `module.exports = \`\n${exampleContract}\`;\n`, 'utf-8');
+//const exampleContract = fs.readFileSync(__dirname + '/../contracts/ExampleContract.aes', 'utf-8');
+//fs.writeFileSync(__dirname + '/../ExampleContract.aes.js', `module.exports = \`\n${exampleContract}\`;\n`, 'utf-8');
---
+++ test/tokenSaleTest.js
@@ -0,0 +1,62 @@
+const assert = require('chai').assert
+const {readFileRelative} = require('aeproject-utils/utils/fs-utils');
+const {defaultWallets: wallets} = require('aeproject-config/config/node-config.json');
+
+const {Universal, MemoryAccount, Node} = require('@aeternity/aepp-sdk');
+const TOKEN_SALE = readFileRelative('./contracts/TokenSale.aes', 'utf-8');
+const TOKEN = readFileRelative('./contracts/FungibleTokenCustom.aes', 'utf-8');
+
+const config = {
+  url: 'http://localhost:3001/',
+  internalUrl: 'http://localhost:3001/',
+  compilerUrl: 'http://localhost:3080'
+};
+
+describe('TokenSale Contract', () => {
+  let client, contract, token;
+
+  before(async () => {
+    client = await Universal({
+      nodes: [{
+        name: 'devnetNode',
+        instance: await Node(config)
+      }],
+      accounts: [MemoryAccount({
+        keypair: wallets[0]
+      })],
+      networkId: 'ae_devnet',
+      compilerUrl: config.compilerUrl
+    });
+  });
+
+  it('Deploy Contract', async () => {
+    contract = await client.getContractInstance(TOKEN_SALE);
+    const init = await contract.methods.init();
+    assert.equal(init.result.returnType, 'ok');
+  });
+
+  it('Deploy and set Token', async () => {
+    token = await client.getContractInstance(TOKEN);
+    const init = await token.methods.init("Test Token", 0, "TT", contract.deployInfo.address.replace('ct_', 'ak_'));
+    assert.equal(init.result.returnType, 'ok');
+    const set = await contract.methods.set_token(token.deployInfo.address);
+    assert.equal(set.result.returnType, 'ok');
+  });
+
+  it('Buy Tokens', async () => {
+    const buy = await contract.methods.buy({amount: 2});
+    assert.equal(buy.result.returnType, 'ok');
+
+    const amount = await token.methods.balance(wallets[0].publicKey);
+    assert.equal(amount.decodedResult, 2);
+  });
+
+  it('Sell Tokens', async () => {
+    await token.methods.create_allowance(contract.deployInfo.address.replace('ct_', 'ak_'), 1);
+    const sell = await contract.methods.sell(1);
+    assert.equal(sell.result.returnType, 'ok');
+
+    const amount = await token.methods.balance(wallets[0].publicKey);
+    assert.equal(amount.decodedResult, 1);
+  });
+});
