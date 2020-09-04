84ff77bc0cf8400792b76f3f5e62c415d041f204
Philipp Piwo <github@piwo.space>
2020-09-04 10:25:56 +0200

    expose contract source as modules

--- .scripts/postinstall.js
+++ .scripts/postinstall.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
 
-//const exampleContract = fs.readFileSync(__dirname + '/../contracts/ExampleContract.aes', 'utf-8');
-//fs.writeFileSync(__dirname + '/../ExampleContract.aes.js', `module.exports = \`\n${exampleContract}\`;\n`, 'utf-8');
+const FungibleTokenCustom = fs.readFileSync(__dirname + '/../contracts/FungibleTokenCustom.aes', 'utf-8');
+fs.writeFileSync(__dirname + '/../FungibleTokenCustom.aes.js', `module.exports = \`\n${FungibleTokenCustom.replace(/`/g, "\\`")}\`;\n`, 'utf-8');
+
+const TokenSale = fs.readFileSync(__dirname + '/../contracts/TokenSale.aes', 'utf-8');
+fs.writeFileSync(__dirname + '/../TokenSale.aes.js', `module.exports = \`\n${TokenSale.replace(/`/g, "\\`")}\`;\n`, 'utf-8');
+
+const WordRegistry = fs.readFileSync(__dirname + '/../contracts/WordRegistry.aes', 'utf-8');
+fs.writeFileSync(__dirname + '/../WordRegistry.aes.js', `module.exports = \`\n${WordRegistry.replace(/`/g, "\\`")}\`;\n`, 'utf-8');
